11785fce070f05b84e79846119b3b78bfb1f35cc
ali6465 <45683275+ali6465@users.noreply.github.com>
2021-02-19 13:20:02 -0700

    register page created

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Register from "@/views/Register.vue";
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,14 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/register',
+    name: Register,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "service" */ '../views/Register.vue')
   },
   {
     path: '/test',
---
+++ src/views/Register.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <cdl_header/>
+    <navbar/>
+
+
+    <div class="auth-wrapper">
+      <div class="auth-inner">
+        <RegisterForm/>
+      </div>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+  import navbar from "@/components/navbar";
+  import cdl_header from "@/components/cdl_header";
+  import RegisterForm from "@/components/RegisterForm";
+
+export default {
+  name: "Register",
+  components: {
+    navbar,
+    cdl_header,
+    RegisterForm
+  }
+
+}
+</script>
+
+<style scoped>
+  * {
+      box-sizing: border-box;
+  }
+
+  .auth-wrapper{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: left;
+    padding-top: 50px;
+  }
+
+  .auth-inner{
+    width: 450px;
+    margin: auto;
+    background: aqua;
+    padding: 40px 55px 40px 55px;
+    border-radius: 15px;
+    transition: all .3s;
+  }
+</style>
