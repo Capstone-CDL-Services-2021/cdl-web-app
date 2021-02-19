a40dbc1e142be20d8fcfcf4487f85014bd28ab76
David Do <55934975+D-vidDo@users.noreply.github.com>
2021-02-19 13:28:33 -0700

    register page frontend implemented

---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,64 @@
+<template>
+  <form @submit.prevent="registerHandler">
+    <h1>Register</h1>
+
+    <div class = "form-group">
+      <label>First Name</label>
+      <input type="text" class="form-control" v-model="first_name" placeholder="First Name"/>
+    </div>
+
+    <div class = "form-group">
+      <label>Last Name</label>
+      <input type="text" class="form-control" v-model="last_name" placeholder="Last Name"/>
+    </div>
+
+    <div class = "form-group">
+      <label>Email</label>
+      <input type="email" class="form-control" v-model="email" placeholder="Email"/>
+    </div>
+
+    <div class = "form-group">
+      <label>Password</label>
+      <input type="password" class="form-control" v-model="password" placeholder="Password"/>
+    </div>
+
+    <div class = "form-group">
+      <label>Confirm Password</label>
+      <input type="password" class="form-control" v-model="password_confirm" placeholder="Password"/>
+    </div>
+
+    <button class="btn-primary btn-block">Sign Up</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "RegisterForm",
+  data() {
+    return {
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+      password_confirm: ''
+
+    }
+  },
+  methods: {
+    registerHandler(){
+      const data = {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        email: this.email,
+        password: this.password,
+        password_confirm: this.password_confirm
+      };
+      console.log(data);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,19 +1,23 @@
 <template>
-  <b-navbar class="navbar navbar-light" style="background-color: green;">
+  <b-navbar class="navbar navbar-light" style="background-color: green;" sticky>
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
 
-        <b-button-group size="lg">
+        <!--        <b-button-group size="lg">-->
 
-          <!--            <b-button variant="outline-light" @click=redirect>Home</b-button>-->
-          <b-button variant="outline-light" v-on:click="redirect('/')" >Home</b-button>
-          <b-button variant="outline-light" v-on:click="redirect('/services')" >Services</b-button>
-          <b-button variant="outline-light">Schedule</b-button>
-          <b-button variant="outline-light">Testimonials</b-button>
+        <!--            <b-button variant="outline-light" @click=redirect>Home</b-button>-->
+        <b-button variant="light" v-on:click="redirect('/')">Home</b-button>
+        <b-button variant="outline-light" v-on:click="redirect('/services')">Services</b-button>
+        <b-button variant="outline-light" v-on:click="redirect('/test')">Schedule</b-button>
+        <b-button variant="outline-light" v-on:click="redirect('/register')">Testimonials</b-button>
 
-        </b-button-group>
+
+        <a href="#" class="btn btn-white btn-animation-1" v-on:click="redirect('/book')">Request a Service</a>
+
+
+        <!--        </b-button-group>-->
       </b-navbar-nav>
 
     </b-collapse>
@@ -23,10 +27,8 @@
 <script>
 export default {
   name: "navbar",
-  data(){
-    return {
-
-    }
+  data() {
+    return {}
   },
   methods: {
     redirect(id) {
@@ -38,5 +40,85 @@ export default {
 </script>
 
 <style scoped>
+.nav-item.nav-item.nav-item a {
+  color: white;
+  padding: 20px;
+  border: 3px solid white;
 
+}
+
+body {
+  background: lightblue;
+  text-align: center;
+  box-sizing: border-box;
+  font-family: "Lato", Sans-serif;
+  /*position:relative;*/
+}
+
+.btn:link,
+.btn:visited {
+  text-decoration: none;
+  text-transform: uppercase;
+  position: relative;
+  top: 0;
+  left: 0;
+  color: green;
+  /*padding:20px 40px;*/
+  border-radius: 100px;
+  display: inline-block;
+  transition: all .5s;
+  font-family: "Corbel Light";
+  margin-left: 84rem;
+}
+
+.btn-white {
+  background: white;
+  color: #000;
+
+}
+
+.btn:active {
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+  transform: translateY(-1px);
+}
+
+.btn-bottom-animation-1 {
+  animation: comeFromBottom 1s ease-out .8s;
+}
+
+.btn::after {
+  content: "";
+  text-decoration: none;
+  text-transform: uppercase;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  border-radius: 100px;
+  display: inline-block;
+  z-index: -1;
+  transition: all .5s;
+
+}
+
+.btn-white::after {
+  background: white;
+}
+
+.btn-animation-1:hover::after {
+  transform: scaleX(1.4) scaleY(1.6);
+  opacity: 0;
+}
+
+@keyframes comeFromBottom {
+  0% {
+    opacity: 0;
+    transform: translateY(40px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
 </style>
