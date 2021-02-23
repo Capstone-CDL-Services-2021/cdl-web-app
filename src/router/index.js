8757aed9893ef58a752ad1a16f3eef4fc821a581
YeonInn <Jlam182000@gmail.com>
2021-02-23 16:18:30 -0700

    Manager Booking Page

--- src/router/index.js
+++ src/router/index.js
@@ -49,6 +49,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "service" */ '../views/manager/managerService.vue')
+  },
+  {
+    path: '/managerBookings',
+    name: 'ManagerBookings',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "service" */ '../views/manager/managerBookings.vue')
   }
 ]
 
