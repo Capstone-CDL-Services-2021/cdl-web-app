0185a4769944df185891617b08aa131fb7707618
jobanh <jobanh@users.noreply.github.com>
2021-02-23 17:43:55 -0700

    changes done 2/17/21

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -9,7 +9,12 @@
         <b-button variant="outline-light" v-on:click="redirect('/test')">Schedule</b-button>
         <b-button variant="outline-light" v-on:click="redirect('/register')">Testimonials</b-button>
 
-        <a href="/book" class="btn btn-white btn-animation-1" v-on:click="redirect('/book')">Request a Service</a>
+       <!-- <a href="/book" class="btn btn-white btn-animation-1" v-on:click="redirect('/book')">Request a Service</a> -->
+      </b-navbar-nav>
+      <b-navbar-nav class = "ml-auto">
+
+          <b-button variant="outline-light" right>Request a Service</b-button>
+
       </b-navbar-nav>
 
     </b-collapse>
@@ -47,71 +52,71 @@ body {
   /*position:relative;*/
 }
 
-.btn:link,
-.btn:visited {
-  text-decoration: none;
-  text-transform: uppercase;
-  position: relative;
-  top: 0;
-  left: 0;
-  color: green;
-  /*padding:20px 40px;*/
-  border-radius: 100px;
-  display: inline-block;
-  transition: all .5s;
-  font-family: "Corbel Light";
-  margin-left: 84rem;
-}
+/*.btn:link,*/
+/*.btn:visited {*/
+/*  text-decoration: none;*/
+/*  text-transform: uppercase;*/
+/*  position: relative;*/
+/*  top: 0;*/
+/*  left: 0;*/
+/*  color: green;*/
+/*  !*padding:20px 40px;*!*/
+/*  border-radius: 100px;*/
+/*  display: inline-block;*/
+/*  transition: all .5s;*/
+/*  font-family: "Corbel Light";*/
 
-.btn-white {
-  background: white;
-  color: #000;
+/*}*/
+
+/*.btn-white {*/
+/*  background: white;*/
+/*  color: #000;*/
 
 
-}
+/*}*/
 
-.btn:active {
-  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
-  transform: translateY(-1px);
-}
+/*.btn:active {*/
+/*  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);*/
+/*  transform: translateY(-1px);*/
+/*}*/
 
-.btn-bottom-animation-1 {
-  animation: comeFromBottom 1s ease-out .8s;
-}
+/*.btn-bottom-animation-1 {*/
+/*  animation: comeFromBottom 1s ease-out .8s;*/
+/*}*/
 
-.btn::after {
-  content: "";
-  text-decoration: none;
-  text-transform: uppercase;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  border-radius: 100px;
-  display: inline-block;
-  z-index: -1;
-  transition: all .5s;
+/*.btn::after {*/
+/*  content: "";*/
+/*  text-decoration: none;*/
+/*  text-transform: uppercase;*/
+/*  position: absolute;*/
+/*  width: 100%;*/
+/*  height: 100%;*/
+/*  top: 0;*/
+/*  left: 0;*/
+/*  border-radius: 100px;*/
+/*  display: inline-block;*/
+/*  z-index: -1;*/
+/*  transition: all .5s;*/
 
-}
+/*}*/
 
-.btn-white::after {
-  background: white;
-}
+/*.btn-white::after {*/
+/*  background: white;*/
+/*}*/
 
-.btn-animation-1:hover::after {
-  transform: scaleX(1.4) scaleY(1.6);
-  opacity: 0;
-}
+/*.btn-animation-1:hover::after {*/
+/*  transform: scaleX(1.4) scaleY(1.6);*/
+/*  opacity: 0;*/
+/*}*/
 
-@keyframes comeFromBottom {
-  0% {
-    opacity: 0;
-    transform: translateY(40px);
-  }
-  100% {
-    opacity: 1;
-    transform: translateY(0);
-  }
-}
+/*@keyframes comeFromBottom {*/
+/*  0% {*/
+/*    opacity: 0;*/
+/*    transform: translateY(40px);*/
+/*  }*/
+/*  100% {*/
+/*    opacity: 1;*/
+/*    transform: translateY(0);*/
+/*  }*/
+/*}*/
 </style>
