c30c214613a99387c461c2e1525b8d80f102548e
YeonInn <Jlam182000@gmail.com>
2021-03-07 16:41:51 -0700

    Pushed contact us enhancements to master from my branch

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -8,60 +8,65 @@
 
     <template>
       <div>
-        <b-form @submit="onSubmit">
-          <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-            <b-form-input
+        <div class="col-sm-20" style="padding: 0px 100px 0px 100px">
+          <b-form @submit="onSubmit">
+            <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+              <b-form-input
+                  id="input-2"
+                  v-model="form.name"
+                  placeholder="Enter Full Name"
+                  required></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+                id="input-group-1"
+                label="Email address:"
+                label-for="input-1"
+                description="We'll never share your email with anyone else.">
+
+              <b-form-input
+                  id="input-1"
+                  v-model="form.email"
+                  type="email"
+                  placeholder="Enter email"
+                  required></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+                id="input-group-2"
+                label="Question"
+                label-for="input-2"
+                description="Please put your Question Below!">
+            </b-form-group>
+
+            <b-form-textarea
                 id="input-2"
-                v-model="form.name"
-                placeholder="Enter Full Name"
-                required></b-form-input>
-
-          </b-form-group>
-
-          <b-form-group
-              id="input-group-1"
-              label="Email address:"
-              label-for="input-1"
-              description="We'll never share your email with anyone else.">
-
-            <b-form-input
-                id="input-1"
-                v-model="form.email"
-                type="email"
-                placeholder="Enter email"
-                required></b-form-input>
-
-          </b-form-group>
-
-          <b-form-group
-            id="input-group-2"
-            label="Question"
-            label-for="input-2"
-            description="Please put your Question Below!">
-          </b-form-group>
-
-          <b-form-input
-            id="input-2"
-            v-model="form.question"
-            placeholder="Enter Question Here!"
-            required></b-form-input>
-          <b-button type="submit" variant="primary">Submit</b-button>
-        </b-form>
+                v-model="form.question"
+                placeholder="Enter Question Here!"
+                rows="3"
+                max-rows="6"
+                required></b-form-textarea>
+            <br/>
+            <b-button type="submit" variant="primary">Submit</b-button>
+          </b-form>
+        </div>
       </div>
     </template>
-
+    <contact-us/>
   </div>
 </template>
 
 <script>
 import navbar from "@/components/navbar";
 import cdl_header from "@/components/cdl_header";
+import contactUs  from "@/components/contactUs";
 
 export default {
   name: "Service",
   components: {
     navbar,
-    cdl_header
+    cdl_header,
+    contactUs
   },
   data() {
     return {
@@ -104,6 +109,11 @@ body {
 .align-bottom {
   text-align: right;
   text-anchor: end;
+}
+
+.col-sm-20 {
+  text-align: center;
+  text-anchor: middle;
 }
 
 </style>
