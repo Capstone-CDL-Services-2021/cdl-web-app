efc117e4104c451d430e7bf63728a74b5366834a
Exenpai <ju5t1n53rr4n0@gmail.com>
2021-03-28 16:45:03 -0600

    Update Account.vue

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -16,53 +16,110 @@
             <p>Full Name: {{ user.first_name }} {{ user.last_name }}</p>
             <p>Email: {{ user.email }}</p>
           </div>
+
+          <!--Update User Information -->
+          <b-button variant="primary" v-b-modal.modal-update>Update User Info</b-button>
+          <b-modal
+              id="modal-update"
+              ref="modal"
+              title="Update User Info"
+              @show="updateModal"
+              @hidden="updateModal"
+              @ok="handleOkUpdate"
+
+          >
+            <b-form ref="form" @submit.stop.prevent="handleSubmitUpdate">
+              <b-form-group
+                  label="Change First Name:"
+                  label-for="change_firstname"
+                  invalid-feedback="First Name is required"
+                  label-cols-sm="4"
+                  :state="newFirstnameState"
+              >
+                <b-form-input
+                    id="change_firstname"
+                    v-model="new_firstname"
+                    :state="newFirstnameState"
+                    required
+                ></b-form-input>
+              </b-form-group>
+              <b-form-group
+                  label="Change Last Name:"
+                  label-for="change_lastname"
+                  invalid-feedback="Last Name is required"
+                  label-cols-sm="4"
+                  :state="newLastnameState"
+              >
+                <b-form-input
+                    id="change_lastname"
+                    v-model="new_lastname"
+                    :state="newLastnameState"
+                    required
+                ></b-form-input>
+              </b-form-group>
+              <b-form-group
+                  label="Change Email:"
+                  label-for="change_email"
+                  invalid-feedback="Email is required"
+                  label-cols-sm="4"
+                  :state="newEmailState"
+              >
+                <b-form-input
+                    id="change_email"
+                    v-model="new_email"
+                    :state="newEmailState"
+                    required
+                ></b-form-input>
+              </b-form-group>
+            </b-form>
+            {{message_update}}
+          </b-modal>
         </b-jumbotron>
 
         <b-jumbotron text-variant="black" border-variant="dark">
           <!-- Delete Account UI -->
-          <b-button variant="danger" @click="$bvModal.show('modal-delete')"> Delete Account</b-button>
-          <b-modal id="modal-delete">
-            <template #modal-header="{}">
-              <h5>Delete Account</h5>
-            </template>
-
-            <h6>If you would like to delete your account permanently please enter "Confirm Delete Account" in the text
-              box below </h6>
-
-            <br/>
-
-            <b-form ref="form">
+          <b-button variant="danger" v-b-modal.modal-delete>Delete Account</b-button>
+          <b-modal
+              id="modal-delete"
+              ref="modal"
+              title="Deleting Account"
+              @show="deleteModal"
+              @hidden="deleteModal"
+              @ok="handleOkDelete"
+          >
+            <p>If you would like to delete your account permanently please enter "Confirm Delete Account" in the text
+              box below</p>
+            <b-form ref="form" @submit.stop.prevent="handleSubmitDelete">
               <b-form-group
                   label="Delete Account:"
-                  label-for="confirm_delete"
+                  label-for="delete_form"
                   invalid-feedback="Account Deletion Message Required"
                   label-cols-sm="4"
-                  :state="deleteCon"
+                  :state="confirmDeleteState"
               >
-
                 <b-form-input
-                    id="deleteConfirm"
-                    v-model="delete_message"
-                    :state="deleteCon"
+                    id="delete_form"
+                    v-model="confirm_delete"
+                    :state="confirmDeleteState"
                     required
                 />
-
               </b-form-group>
             </b-form>
           </b-modal>
-          <p>{{ delMessage }}</p>
+          <p>{{ message_delete }}</p>
+
 
           <!-- Change password stuff  -->
-          <b-button variant="warning" v-b-modal.modal-prevent-closing>Change Password</b-button>
+          <b-button variant="warning" v-b-modal.modal-password>Change Password</b-button>
           <b-modal
-              id="modal-prevent-closing"
+              id="modal-password"
               ref="modal"
               title="Changing Password"
               @show="resetModal"
               @hidden="resetModal"
-              @ok="handleOk"
+              @ok="handleOkPassword"
           >
-            <b-form ref="form" @submit.stop.prevent="handleSubmit">
+            <b-form ref="form" @submit.stop.prevent="handleSubmitPassword">
               <b-form-group
                   label="New Password:"
                   label-for="new_password_input"
@@ -93,8 +150,7 @@
               </b-form-group>
             </b-form>
           </b-modal>
-          <p>{{ message }}</p>
-
+          <p>{{ message_password }}</p>
 
           <!-- View Order History  -->
           <b-button variant="primary" @click="$bvModal.show('modal-order')">View Orders</b-button>
@@ -115,69 +171,16 @@
             </template>
           </b-modal>
           <p></p>
-
-          <!-- Change Account Info  -->
-          <b-button variant="primary" @click="$bvModal.show('modal-update')">Update Account Info</b-button>
-          <b-modal id="modal-update">
-            <template #modal-header="{}">
-              <h5>Account Information</h5>
-            </template>
-
-            <p>Full Name: {{ user.first_name }} {{ user.last_name }}</p>
-            <p>Email: {{ user.email }}</p>
-
-            <b-button size="sm" variant="primary" v-b-modal.update>Update</b-button>
-
-            <!-- added button for changes -->
-            <b-modal id="update" size="md" title="Update User Information">
-              <b-form ref="form">
-                <b-form-group
-                    label="Change Name:"
-                    label-for="name_change"
-                    label-cols-sm="4"
-                >
-                  <b-form-input
-                      id="new_full_name"
-                      v-model="new_name"
-                  ></b-form-input>
-
-                </b-form-group>
-
-                <b-form-group
-                    label="Change Email:"
-                    label-for="email_new"
-                    label-cols-sm="4"
-                >
-                  <b-form-input
-                      id="email_change"
-                      v-model="email_new"
-                  ></b-form-input>
-
-                </b-form-group>
-                <p>{{ updateMessage }}</p>
-              </b-form>
-            </b-modal>
-
-            <template #modal-footer="{ ok, cancel }">
-              <b-button size="sm" variant="success" @click="ok()">Ok</b-button>
-              <b-button size="sm" variant="danger" @click="cancel()">Close</b-button>
-            </template>
-          </b-modal>
-
-
         </b-jumbotron>
-
       </div>
-
     </b-jumbotron>
-
   </div>
-
 </template>
 
 <script>
 import cdl_header from "@/components/cdl_header";
 import navbar from "@/components/navbar";
+import Home from "@/views/Home";
 import {mapGetters} from 'vuex';
 
 export default {
@@ -188,24 +191,71 @@ export default {
   },
   data() {
     return {
-      message: '',
-      delMessage: '',
-      delete_message: '',
-      updateMessage: '',
+      //Update User Info form
+      new_firstname: '',
+      newFirstnameState: '',
+      new_lastname: '',
+      newLastnameState: '',
+      new_email: '',
+      newEmailState: '',
+      message_update: '',
+      // Delete form
+      confirm_delete: '',
+      message_delete: '',
+      confirmDeleteState: null,
+      // Reset Password form
       new_password: '',
       newPasswordState: null,
       confirm_password: '',
-      confirmPasswordState: null
+      confirmPasswordState: null,
+      message_password: ''
     }
   },
   methods: {
-    checkFormValidity() {
-      //const valid = this.$refs.form.checkValidity()
-      if (this.new_password === this.confirm_password) {
-        return true
-      } else {
-        return false
+    // Update Form functionality
+    checkFormValidityUpdate() {
+      const valid = this.$refs.form.checkValidity()
+      this.newFirstnameState = valid
+      this.newLastnameState = valid
+      this.newEmailState = valid
+      return valid
+    },
+    updateModal() {
+      this.new_firstname = ''
+      this.new_lastname = ''
+      this.new_email = ''
+      this.message_update = ''
+      this.newFirstnameState = null
+      this.newLastnameState = null
+      this.newEmailState = null
+    },
+    handleOkUpdate(bvModalEvt) {
+      // Prevent modal from closing
+      bvModalEvt.preventDefault()
+      // Trigger submit handler
+      this.handleSubmitUpdate()
+    },
+    handleSubmitUpdate() {
+      // Exit when the form isn't valid
+      if (!this.checkFormValidityUpdate()) {
+        return
       }
+      else {
+        // Print Account successfully updated
+        this.message_update = "User Info successfully updated"
+      }
+      // Hide the modal manually
+      this.$nextTick(() => {
+        this.$bvModal.hide('modal-update')
+      })
+    },
+
+    // Password form functionality
+    checkFormValidityPassword() {
+      const valid = this.$refs.form.checkValidity()
+      this.newPasswordState = valid
+      this.confirmPasswordState = valid
+      return valid;
     },
     resetModal() {
       this.new_password = ''
@@ -213,24 +263,67 @@ export default {
       this.newPasswordState = null
       this.confirmPasswordState = null
     },
-    handleOk(bvModalEvt) {
+    handleOkPassword(bvModalEvt) {
       // Prevent modal from closing
       bvModalEvt.preventDefault()
       // Trigger submit handler
-      this.handleSubmit()
+      this.handleSubmitPassword()
     },
-    handleSubmit() {
+    handleSubmitPassword() {
       // Exit when the form isn't valid
-      if (!this.checkFormValidity()) {
-        this.message = "Error: Password did not match"
-      } else {
+      if (!this.checkFormValidityPassword()) {
+        return
+      }
+      // Check if password match
+      else if (this.new_password !== this.confirm_password) {
+        this.message_password = "Error: Password did not match"
+      }
+      else {
         // Print the new password
-        this.message = "Password successfully changed to \"" + this.new_password + "\""
+        this.message_password = "Password successfully changed to \"" + this.new_password + "\""
       }
       // Hide the modal manually
       this.$nextTick(() => {
-        this.$bvModal.hide('modal-prevent-closing')
+        this.$bvModal.hide('modal-password')
       })
+    },
+
+    // Delete Form functionality
+    checkFormValidityDelete() {
+      const valid = this.$refs.form.checkValidity()
+      this.confirmDeleteState = valid
+      return valid
+    },
+    deleteModal() {
+      this.confirm_delete = ''
+      this.message_delete = ''
+      this.confirmDeleteState = null
+    },
+    handleOkDelete(bvModalEvt) {
+      // Prevent modal from closing
+      bvModalEvt.preventDefault()
+      // Trigger submit handler
+      this.handleSubmitDelete()
+    },
+    handleSubmitDelete() {
+      // Exit when the form isn't valid
+      if (!this.checkFormValidityDelete()) {
+        return
+      }
+      // Check if Confirm Delete input matches
+      else if (this.confirm_delete !== "Confirm Delete Account"){
+        this.message_delete = "Error: Account Deletion Message did not match"
+      }
+      else {
+        // Print Account successfully deleted
+        this.message_delete = "Account successfully deleted"
+      }
+      // Hide the modal manually
+      this.$nextTick(() => {
+        this.$bvModal.hide('modal-delete')
+      })
+      // Go back Home
+      this.$router.push(Home);
     }
   },
   computed: {
