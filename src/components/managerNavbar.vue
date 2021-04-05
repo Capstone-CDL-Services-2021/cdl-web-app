f6c92cc9407321f6a1e4453548fdf7fea6520f52
jobanh <jobanh@users.noreply.github.com>
2021-04-05 14:38:52 -0600

    deleted Manager booking from NavBar

--- src/components/managerNavbar.vue
+++ src/components/managerNavbar.vue
@@ -8,7 +8,6 @@
         <b-button variant="outline-light" v-on:click="redirect('/managerService')">Services</b-button>
         <b-button variant="outline-light" v-on:click="redirect('/managerHome')">Schedule</b-button>
         <b-button variant="outline-light" v-on:click="redirect('/testimonials')">Testimonials</b-button>
-        <b-button variant="outline-light" v-on:click="redirect('/managerBookings')">Manager Bookings</b-button>
         <b-button variant="outline-light" v-on:click="redirect('/managerHome/')">Reports</b-button>
         <b-button variant="outline-light" v-on:click="redirect('/managerHome')">Edit About Us</b-button>
         <b-button variant="outline-light" v-on:click="redirect('/managerUserAccounts')">Manager User Accounts</b-button>
