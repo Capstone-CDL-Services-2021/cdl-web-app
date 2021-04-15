ee47a94164815478f20612d7ee9c10a35ac53b6e
jobanh <jobanh@users.noreply.github.com>
2021-04-14 19:28:35 -0600

    userCheck javadoc

--- src/components/userCheck.vue
+++ src/components/userCheck.vue
@@ -1,5 +1,7 @@
+<!--This component is called whenever a user is logged in, it will go through a check if it's a manager or not.-->
 <template>
   <div v-if="user">
+<!--    if user is a manager, it will redirect to the manager home page-->
     <div v-if="user.email == 'manager@cdlservices.com'">{{ redirect('/managerHome')}}<manager-navbar/></div>
     <div v-else> <navbar/></div></div>
   <div v-else> <navbar/>
@@ -7,11 +9,16 @@
 </template>
 
 <script>
+/**
+ * import components, views and dependencies
+ */
 import managerNavbar from "@/components/managerNavbar";
 import navbar from "@/components/navbar";
 import {mapGetters} from "vuex";
 
-
+/**
+ * export components, views and methods from the imports
+ */
 export default {
   name: "userCheck",
   components: {
@@ -21,6 +28,10 @@ export default {
     ...mapGetters(['user'])
   },
   methods: {
+    /**
+     * this method is to redirect based on the id parameter input
+     * @param id a String value that is a path
+     */
     redirect(id) {
       this.$router.push(id)
     }},
