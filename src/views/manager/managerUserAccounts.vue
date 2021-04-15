bc4654949687f518428b2506fdcff5a30c99887c
Exenpai <ju5t1n53rr4n0@gmail.com>
2021-04-14 18:55:21 -0600

    javadoc for account and manageuseraccount

--- src/views/manager/managerUserAccounts.vue
+++ src/views/manager/managerUserAccounts.vue
@@ -6,13 +6,12 @@
 
     <b-jumbotron bg-variant="dark" border-variant="dark">
       <div hidden>
-        {{getAllUsers}}
+        <!--Invoke the compute method-->
+        {{ getAllUsers }}
       </div>
       <div>
         <b-jumbotron>
           <h1 style="text-align: center">Registered Users</h1>
-          <!-- Version 2 with universal options at the top -->
-          <!--                    <b-button size="sm" variant="primary" v-on:click="redirect('/managerUserAccountHistory')">View</b-button>-->
           <b-button size="sm" variant="danger" @click="resetPassword">Reset</b-button>
           <b-button size="sm" variant="secondary" @click="blockUnblock">Block/Unblock</b-button>
           <br><br>
@@ -25,6 +24,7 @@
               selectable
               @row-selected="onRowSelected">
 
+            <!--templates for specific columns indicated by #cell-->
             <template #cell(selected)="{ rowSelected }">
               <template v-if="rowSelected">
                 <span aria-hidden="true">&check;</span>
@@ -44,7 +44,7 @@
               </template>
             </template>
           </b-table>
-          <h4>{{message}}</h4>
+          <h4>{{ message }}</h4>
         </b-jumbotron>
       </div>
 
@@ -55,16 +55,25 @@
 </template>
 
 <script>
+/**
+ * import components, views and dependencies
+ */
 import managerHeader from "@/components/managerHeader";
 import managerNavbar from "@/components/managerNavbar";
 import axios from "axios";
 
+/**
+ * export components, views and methods from the imports
+ */
 export default {
   name: "manageUserAccounts",
   components: {
     managerHeader,
     managerNavbar
   },
+  /**
+   *  return data to declared and instantiated variables
+   */
   data() {
     return {
       message: '',
@@ -91,24 +100,58 @@ export default {
         {
           first_name: '',
           last_name: '',
-          blocked:''
+          blocked: ''
         }
       ]
     }
   },
+  /**
+   * Method for reactive dependencies
+   */
   computed: {
+    /**
+     * getAllUsers - grabs all the users from the Database
+     *               except for the admin account and insert
+     *               into user.Info to generate the table
+     *
+     * @returns {Promise<AxiosResponse<any>>}
+     * invoke the method from the back-end api to grab data from the database
+     */
     getAllUsers() {
       // eslint-disable-next-line vue/no-async-in-computed-properties
       return axios.get('getAllUsers').then(response => this.userInfo = response.data)
     }
   },
+  /**
+   * Method for reactive dependencies
+   */
   methods: {
+    /**
+     * onRowSelected - method that checks which row is selected
+     *                 in the table, grabs the values in items
+     *                 and stores into selectedRow
+     *
+     * @param items
+     * the values selected in the table that is then stored
+     * into selectedRow
+     */
     onRowSelected(items) {
       this.selectedRow = items
     },
+    /**
+     * redirect - Change address bar with id value
+     *
+     * @param id
+     * id - the address path
+     */
     redirect(id) {
       this.$router.push(id)
     },
+    /**
+     * resetPassword - a method that builds a custom modal with methods stored
+     *                 inside which triggers certain event depending on what
+     *                 button pressed
+     */
     resetPassword() {
       this.$bvModal.msgBoxConfirm('Please confirm that you want to reset this user\'s password.', {
         title: 'Please Confirm',
@@ -121,12 +164,21 @@ export default {
         hideHeaderClose: false,
         centered: true,
       })
+          // if button YES is pressed invoke this method
           .then(value => {
             if (value === true) {
               this.resetPass();
             }
           })
     },
+    /**
+     * resetPass - resets the password for selected user from the table.
+     *             User receives email with a link to reset their password
+     *
+     * @returns {Promise<void>}
+     * returns nothing, returns to page and cancels
+     * whole modal event
+     */
     async resetPass() {
       try {
         const response = await axios.post('forgot', {
@@ -140,6 +192,12 @@ export default {
         this.message = '';
       }
     },
+    /**
+     * blockUnblock -  a method that builds a custom modal with methods stored
+     *                 inside which triggers certain event depending on what
+     *                 button pressed
+     *
+     */
     blockUnblock() {
       this.block = ''
       this.$bvModal.msgBoxConfirm('Block/Unblock user?.', {
@@ -153,12 +211,20 @@ export default {
         hideHeaderClose: false,
         centered: true
       })
+          // if button YES is pressed invoke this method
           .then(value => {
             if (value === true) {
               this.toggleBlocked();
             }
           })
     },
+    /**
+     * toggleBlocked - block or unblock the selected user from the tables using their
+     *                 id. Changes the value in the database for blocked to 1 to block,
+     *                 or change the value to 0 to unblock.
+     *
+     * @returns {Promise<void>}
+     */
     async toggleBlocked() {
       try {
         const response = await axios.post('toggleBlocked', {
@@ -167,8 +233,8 @@ export default {
         });
         console.log(response);
         setTimeout(location.reload.bind(location), 500);
-      }catch(e){
-        this.error ='Error occurred';
+      } catch (e) {
+        this.error = 'Error occurred';
       }
     }
   }
