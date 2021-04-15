d3b72d06eef4121cdaa95f201ed633308df7cee5
Exenpai <ju5t1n53rr4n0@gmail.com>
2021-04-14 19:31:22 -0600

    update app.vue, axios.js, main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+/**
+ * import the dependencies
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
@@ -8,12 +11,26 @@ import router from './router'
 import './axios'
 import store from './vuex'
 
+/**
+ * Vue using Vue-Bootstrap plugins
+ */
 Vue.use(ImagePlugin)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
+/**
+ * Set tip information to false
+ * Console log with tips won't print
+ *
+ * @type {boolean}
+ */
 Vue.config.productionTip = false
 
+/**
+ * Create a Vue instance with dependencies (Vue-Router and Store)
+ * Renders the Virtual DOM with App.vue and mounts to the index.html
+ * where <div id="app">.
+ */
 new Vue({
   router,
   store,
